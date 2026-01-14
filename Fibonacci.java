3f2d1d7c71a13dee1aa297686caea269781e1f02
prema-singanal <prema29singanal@gmail.com>
2026-01-14 20:26:00 +0530

    Write a Program to Find Sum of Fibonacci Series Number

---
+++ Fibonacci.java
@@ -0,0 +1,50 @@
+public class Fibonacci {
+    public static void printFibonacciSeries(int n){
+    for(int i = 0 ; i < n ; i++ ){
+        System.out.print(fibonacciIterative(i) + " ");
+    }
+}
+
+    public static int fibonacciIterative(int n) {
+     if ( n == 0 || n == 1 ){
+         return n;
+     }
+     int prev = 0;
+     int curr = 1;
+     for(int i=2 ; i<=n ; i++){
+         int next = prev + curr; // next = 0 + 1 = 1
+         prev = curr; // prev = 1
+         curr = next; // curr = 1
+     }
+     return curr;
+    }
+    // Time Complexity : O(n)
+
+    public static int fibonacci(int n) {
+        if( n == 0 || n == 1 ){
+            return n;
+        }
+        else{
+            return  fibonacci(n-1) + fibonacci(n-2);
+        }
+    }
+    // Time Complexity : O(2^n)
+    public static void main(String[] args) {
+      printFibonacciSeries(10);
+      System.out.println("Fibonacci of 10 is: " + fibonacciIterative(10));
+    }
+
+
+    
+}
+
+// What is fibonacci series? - starts with 0 and 1 and each subsequent number is the sum of the previous two numbers.
+// Swirl pattern , Golden ratio
+
+
+// better approach using iteration
+
+
+
+
+
